71e60d32f75c0cf369b2a5a3bd55203988017884
Walison Filipe <walisonfilipe@hotmail.com>
2019-10-05 20:22:22 -0300

    Fix UserProfile model

--- myproject/core/models.py
+++ myproject/core/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
-        related_name='users'
+        related_name='profile'
     )
-    cpf = models.CharField(max_length=14)
+    cpf = models.CharField(max_length=14, unique=True)
 
     def __str__(self):
         return str(self.user)
