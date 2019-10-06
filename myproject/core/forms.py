0a28c26ed5ea102845f4101687011141178dd3ee
Walison Filipe <walisonfilipe@hotmail.com>
2019-10-05 21:59:18 -0300

    Remove unused code

--- myproject/core/forms.py
+++ myproject/core/forms.py
@@ -1,13 +1,4 @@
-from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-
-
-class AccountForm(forms.Form):
-    cpf = forms.CharField(label='CPF', max_length=14)
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        fields = ('cpf', 'password')
 
 
 class LoginForm(AuthenticationForm):
